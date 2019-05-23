3bca67eaa5bde4dd3d9ec695b0a7729e57e5c7f0
jimmy <jimmy.pan@hotmail.com>
2019-05-23 10:36:32 +0800

    移除項目

--- Services/Install.bat
+++
@@ -1,7 +0,0 @@
-
-sc stop BackServer
-sc delete BackServer
-ping -n 5 127.0.0.1>nul
-Services.exe Install
-sc start BackServer
-pause
--- Services/UnInstall.bat
+++
@@ -1,4 +0,0 @@
-sc stop BackServer
-ping -n 2 127.0.0.1>nul
-sc delete BackServer
-pause
